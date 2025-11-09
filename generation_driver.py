fe674b4c49e048f25f067b0dc870541109593810
Joel Horne <jhorne98@protonmail.com>
2025-11-08 20:32:52 -0500

    Command line selection

--- generation_driver.py
+++ generation_driver.py
@@ -2,13 +2,25 @@ import relational_db_setup
 import graph_db_setup
 import graph_extension_setup
 
+import sys
+
 if __name__ == '__main__':
-    #relational_db_setup.mariadb_operations()
-    #relational_db_setup.postgres_operations()
-    #relational_db_setup.aerospike_operations()
-    #graph_db_setup.cypher_operations(graph_db_setup.memgraph_config, "memgraph")
-    graph_db_setup.cypher_operations(graph_db_setup.neo4j_config, "neo4j")
-    #graph_db_setup.dgraph_operations()
-    #graph_extension_setup.aerospike_graph_operations()
-    #graph_extension_setup.maria_oqgraph_operations()
-    #graph_extension_setup.apache_age_operations()
+    if len(sys.argv) > 1:
+        if 'mariadb' in sys.argv:
+            relational_db_setup.mariadb_operations()
+        if 'postgres' in sys.argv:
+            relational_db_setup.postgres_operations()
+        if 'aerospike' in sys.argv:
+            relational_db_setup.aerospike_operations()
+        if 'memgraph' in sys.argv:
+            graph_db_setup.cypher_operations(graph_db_setup.memgraph_config, "memgraph")
+        if 'neo4j' in sys.argv:
+            graph_db_setup.cypher_operations(graph_db_setup.neo4j_config, "neo4j")
+        if 'dgraph' in sys.argv:
+            graph_db_setup.dgraph_operations()
+        if 'aerospike_graph' in sys.argv:
+            graph_extension_setup.aerospike_graph_operations()
+        if 'oqgraph' in sys.argv:
+            graph_extension_setup.maria_oqgraph_operations()
+        if 'apache_age' in sys.argv:
+            graph_extension_setup.apache_age_operations()
