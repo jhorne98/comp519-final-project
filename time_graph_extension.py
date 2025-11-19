b54b6a249e85c3fecf4c4cc6c1c7134e46164119
Joel Horne <jhorne98@protonmail.com>
2025-11-18 20:09:26 -0500

    Add graph extension timer

---
+++ time_graph_extension.py
@@ -0,0 +1,14 @@
+import timeit
+import sys
+
+import mariadb
+import psycopg2
+
+from tables import QUERY_RUNS, DBLength, DBType, DBTypePostgres
+import configs
+
+def time_mariadb_oqgraph_queries():
+    pass
+
+def time_apache_age_operations():
+    pass
