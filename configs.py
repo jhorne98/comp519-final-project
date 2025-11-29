f748518d9287af6ce00663387ed91d35edf4a563
Joel Horne <jhorne98@protonmail.com>
2025-11-28 21:33:11 -0500

    Update configuration file

--- configs.py
+++ configs.py
@@ -10,6 +10,14 @@ mariadb_config = {
     'database': dbname,
 }
 
+mariadb_oqgraph_config = {
+    'host': 'localhost',
+    'port': 3306,
+    'user': user,
+    'password': passwd,
+    'database': 'extensiongraphbench',
+}
+
 postgres_config = {
     'host': 'localhost',
     'port': 5432,
@@ -18,16 +26,26 @@ postgres_config = {
     'dbname': dbname,
 }
 
+apache_age_config = {
+    'host': 'localhost',
+    'port': 5432,
+    'user': user,
+    'password': passwd,
+    'dbname': 'extensiongraphbench'
+}
+
 aerospike_config = {
     'hosts': [('localhost', 3000)]
 }
 
 memgraph_config = {
+    'name': "memgraph",
     'uri': "bolt://localhost:7687",
     'auth': ("", "")
 }
 
 neo4j_config = {
+    'name': "neo4j",
     'uri': "neo4j://localhost",
     'auth': ("neo4j", "Bd8EtstJXINw3yfzzA97")
 }
